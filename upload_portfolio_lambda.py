5e48edee7be3ea6b69233cf4bea7d8409a514b68
Virender Ajmani <virender.ajmani@gmail.com>
2020-05-10 17:30:43 -0500

    adding a lambda file to upload to s3

--- upload_portfolio_lambda.py
+++ upload_portfolio_lambda.py
@@ -1,20 +1,28 @@
+import json
 import boto3
 import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('portfolio.virender.guru')
+    s3 = boto3.resource('s3')
 
-build_bucket = s3.Bucket('portfoliobuild.virender.guru')
+    portfolio_bucket = s3.Bucket('portfolio.virender.guru')
 
-portfolio_zip = StringIO.StringIO()
+    build_bucket = s3.Bucket('portfoliobuild.virender.guru')
 
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_zip = StringIO.StringIO()
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj,nm,ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj,nm,ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Deploy Succeeded!')
+    }
